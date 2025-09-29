3792881e4a5bf1b5c2068aabf39c6981a8bcc9d4
Mouhamed Mbaye <mouhamedmbaye371@gmail.com>
2025-09-29 02:43:13 +0200

    Fix: Update build with correct paths for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Remplacez 'mohamed-nlp-journey' par le nom de votre dépôt GitHub
-  base: "/mmnlp-journey/", 
+  
+  base: "/", 
   plugins: [react()],
   resolve: {
     alias: {
